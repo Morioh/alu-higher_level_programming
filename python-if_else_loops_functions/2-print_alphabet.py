6a1acf15aa7661d5069d8f1322c82b114d2dde57
root <Morioh>
2022-10-28 10:28:57 -0700

    Task 2: I sometimes suffer from insomnia...

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for alpha in range(ord('a'), ord('z') + 1):
-    print("{:c}".format(alpha), end="")
+for alpha in range(ord('a'), ord('z')+1):
+    print("{:c}".format(alpha)), end="")
