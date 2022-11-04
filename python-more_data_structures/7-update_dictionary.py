e09fa7da1016aff3cce9c6aee57706fd7073e71e
root <Morioh>
2022-11-04 13:40:52 -0700

    Task 7: Update dictionary

---
+++ python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# function that replaces or adds key/value in a dictionary
+
+
+def update_dictionary(a_dictionary, key, value):
+    a_dictionary[key] = value
+    return a_dictionary
