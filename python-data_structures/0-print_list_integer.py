2c85423d88870c1374169aa65e68bc042344466d
root <Morioh>
2022-11-03 17:02:33 -0700

    Task 0: Print a list of integers

---
+++ python-data_structures/0-print_list_integer.py
@@ -0,0 +1,6 @@
+#!usr/bin/python3
+# Function that prints all integers of a list
+
+def print_list_integer(my_list=[]):
+    for i in range(0, len(my_list)):
+        print("{:d}".format(my_list[i]))
