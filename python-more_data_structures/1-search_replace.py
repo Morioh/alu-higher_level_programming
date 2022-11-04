b28ce418038b851d21db4d33a531dc97692018d7
root <Morioh>
2022-11-04 12:57:39 -0700

    Task 1: Search and replace

---
+++ python-more_data_structures/1-search_replace.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# Function that replaces all occurrences of an element by another in a new list
+
+
+def search_replace(my_list, search, replace):
+    new_list = my_list[:]
+    for i in range(len(new_list)):
+         if new_list[i] == search:
+             new_list[i] = replace
+    return (new_list)
