94ed74b86347619020751f51996487b77a84b558
root <Morioh>
2022-11-10 19:21:52 -0800

    Task 3: Integers division with debug

---
+++ python-exceptions/3-safe_print_division.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# function that divides 2 integers and prints the result
+
+
+def safe_print_division(a, b):
+    try:
+        div = a / b
+    except (TypeError, ZeroDivisionError):
+        div = None
+    finally:
+        print("Inside result: {}".format(div))
+    return(div)
