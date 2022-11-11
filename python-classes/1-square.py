808429e13275f3788d0a8388347bb8367498fe83
root <Morioh>
2022-11-10 23:00:47 -0800

    Task 1: Square with size

---
+++ python-classes/1-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+# class 'Square' that defines a square by:
+# (based on 0-square.py)
+"""
+    define a class 'Square'
+"""
+
+
+class square:
+    """
+        square with private instance attribute: 'size'
+    """
+
+
+class Square:
+    """
+        square with private instance attribute: 'size'
+    """
+
+    def __init__(self, size):
+        """
+            Args:
+                size (int): size of the new square
+        """
+        self. __size = size
