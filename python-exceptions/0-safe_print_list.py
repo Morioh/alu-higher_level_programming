aa4ef29ccaefc5787e2eda7229ae4dbe68e96a33
root <Morioh>
2022-11-10 18:56:35 -0800

    Task 0: Safe list printing

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# function that prints x elements of a list
+
+
+def safe_print_list(my_list=[], x=0):
+    ret = 0
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end="")
+            ret += 1
+        except IndexError:
+            break
+    print("")
+    return (ret)
