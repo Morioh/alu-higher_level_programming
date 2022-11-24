57b6ca051e92212d404c8b663457e1fb0a1bd3bd
Morioh <m.onyonyi@alustudent.com>
2022-11-24 08:52:29 +0300

    Task 2:  POST an email #0

---
+++ python-network_1/2-post_email.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+# python script that takes in a URL and an email,
+# sends a POST request to the passed URL with the email as a parameter,
+# and displays the body of the response (decoded in utf-8)
+"""
+    send a POST request to the passed URL with the email as a parameter,
+    & display body of the response (decoded in utf-8)
+"""
+import sys
+import urllib.parse
+import urllib.request
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    value = {"email": sys.argv[2]}
+    data = urllib.parse.urlencode(value).encode("ascii")
+
+    request = urllib.request.Request(url, data)
+    with urllib.request.urlopen(request) as response:
+        print(response.read().decode("utf-8"))
