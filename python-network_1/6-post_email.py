dabeec40c37d20feb1a24ef56c2a2520a1a7d62c
Morioh <m.onyonyi@alustudent.com>
2022-11-24 09:04:01 +0300

    Task 6: POST an email #1

---
+++ python-network_1/6-post_email.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+# python script that takes in a URL and an email address,
+# sends a POST request to the passed URL with the email as a parameter,
+# and finally displays the body of the response
+"""
+    send a POST request to the passed URL with the email as a parameter,
+    & displaysthe body of the response
+"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    value = {"email": sys.argv[2]}
+
+    r = requests.post(url, data=value)
+    print(r.text)
