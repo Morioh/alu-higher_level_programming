e8b2a227e0164ebca59e6294c3d27c4e65d5e4b6
Morioh <m.onyonyi@alustudent.com>
2022-11-24 09:12:44 +0300

    Task 9:  My GitHub!

---
+++ python-network_1/10-my_github.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# python script that takes your GitHub credentials (username and password),
+# and uses the 'GitHub API' to display your 'id'
+"""
+    use 'GitHub API to display 'id'
+"""
+import sys
+import requests
+from requests.auth import HTTPBasicAuth
+
+
+if __name__ == "__main__":
+    auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
+    r = requests.get("https://api.github.com/user", auth=auth)
+    print(r.json().get("id"))
