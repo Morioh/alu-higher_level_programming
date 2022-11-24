58a8a7158ba18adf303aa66ffed8f7927fa01eed
Morioh <m.onyonyi@alustudent.com>
2022-11-24 08:55:03 +0300

    Task 3: Error code #0

---
+++ python-network_1/3-error_code.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+# python script that takes in a URL,
+# sends a request to the URL and displays the body of the response
+# (decoded in utf-8)
+"""
+    send a request to the URL & display body of the response
+"""
+import sys
+import urllib.error
+import urllib.request
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    request = urllib.request.Request(url)
+    try:
+        with urllib.request.urlopen(request) as response:
+            print(response.read().decode("ascii"))
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
