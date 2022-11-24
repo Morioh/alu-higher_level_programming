5e68f1414b87f5955738cff2b80e1d7e92c54db0
Morioh <m.onyonyi@alustudent.com>
2022-11-24 08:33:02 +0300

    Task 0: What's my status? #0

---
+++ python-network_1/0-hbtn_status.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# python script that fetches 'https://alu-intranet.hbtn.io/status'
+"""
+    fetch 'https://intranet.hbtn.io/status'
+"""
+import urllib.request
+
+
+if __name__ == "__main__":
+    request = urllib.request.Request("https://intranet.hbtn.io/status")
+    with urllib.request.urlopen(request) as response:
+        body = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
