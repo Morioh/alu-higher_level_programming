a66b564ab33623323205e8b61174d5b59080a94e
Morioh <m.onyonyi@alustudent.com>
2022-12-07 11:28:45 +0200

    Task 1: Divide a Matrix

---
+++ python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+# function that divides all elements of a matrix
+"""
+    Define 'matrix_division' function.
+"""
+
+
+def matrix_divided(matrix, div):
+    """
+        Divide all elements of a matrix.
+        Args:
+            matrix (list): list of lists of ints or floats.
+            div (int/float): divisor.
+        Return:
+            new matrix representing the result of the division.
+    """
+    if (not isinstance(matrix, list) or matrix == [] or
+            not all(isinstance(row, list) for row in matrix) or
+            not all((isinstance(ele, int) or isinstance(ele, float))
+                    for ele in [num for row in matrix for num in row])):
+        raise TypeError("matrix must be a matrix (list of lists) of "
+                        "integers/floats")
+
+    if not all(len(row) == len(matrix[0]) for row in matrix):
+        raise TypeError("Each row of the matrix must have the same size")
+
+    if not isinstance(div, int) and not isinstance(div, float):
+        raise TypeError("div must be a number")
+
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+
+    return ([list(map(lambda x: round(x / div, 2), row)) for row in matrix])
