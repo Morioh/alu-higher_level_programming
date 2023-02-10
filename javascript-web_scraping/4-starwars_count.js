ceb2049fe62c7c2f02592f7fc31ed38eeb6eb65c
Morioh <m.onyonyi@alustudent.com>
2023-02-10 23:08:38 +0200

    Task 4: Star wars Wedge Antilles

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -4,9 +4,9 @@ request(process.argv[2], function (error, response, body) {
   if (!error) {
     const results = JSON.parse(body).results;
     console.log(results.reduce((count, movie) => {
-        return movie.characters.find((character) => character.endsWith('/18/'))
-        ? count + 1:
+      return movie.characters.find((character) => character.endsWith('/18/'))
+        ? count + 1 :
         count;
-        }, 0));
-    }
+    }, 0));
+  }
 });
