1f5928c6ce3f858276ea1dcf84eb236a64e9caf1
Morioh <m.onyonyi@alustudent.com>
2023-02-10 22:00:16 +0200

    Task 9: Add

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
 const add = (a, b) => {
-    if (isNaN(a) || isNaN(b)) {
+  if (isNaN(a) || isNaN(b)) {
     console.log('NaN');
-    } else {
+  } else {
     console.log(a + b);
-    }
+  }
 };
+
 add(parseInt(process.argv[2]), parseInt(process.argv[3]));
