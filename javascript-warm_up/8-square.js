67f0795c0a89dc068ef250fd333ea6e194dac48e
Morioh <m.onyonyi@alustudent.com>
2023-02-10 21:41:35 +0200

    Task 8: Square

---
+++ javascript-warm_up/8-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const size = parseInt(process.argv[2]);
+
+if (isNaN(size)) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < size; i++) {
+    let row = '';
+    for (let j = 0; j < size; j++) {
+      row += 'X';
+    }
+    console.log(row);
+  }
+}
