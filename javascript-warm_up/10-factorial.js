1ebfb7684781f65b5c105c6ddcb1108f52eb2653
Morioh <m.onyonyi@alustudent.com>
2023-02-10 22:06:05 +0200

    Task 10: Factorial

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -3,5 +3,5 @@ function factorial (n) {
     if (isNaN(n) || n < 0) return 1;
     if (n === 0) return 1;
     return n * factorial(n - 1);
-}
-console.log(factorial(parseInt(process.argv[2])));
+  }
+  console.log(factorial(parseInt(process.argv[2])));
