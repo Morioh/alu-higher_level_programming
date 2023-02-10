bef50e4a2cb756ea7b182a98267cf3b6c062064f
Morioh <m.onyonyi@alustudent.com>
2023-02-10 23:17:14 +0200

    Task 6: How many completed?

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -2,15 +2,15 @@
 const request = require('request');
 request(process.argv[2], function (error, response, body) {
   if (!error) {
-      const todos = JSON.parse(body);
-      const completed = {};
-      todos.forEach((todo) => {
-        if (todo.completed && completed[todo.userId] === undefined) {
-          completed[todo.userId] = 1;
-        } else if (todo.completed) {
-          completed[todo.userId] += 1;
-          }
-      });
-        console.log(completed);
+    const todos = JSON.parse(body);
+    const completed = {};
+    todos.forEach((todo) => {
+      if (todo.completed && completed[todo.userId] === undefined) {
+        completed[todo.userId] = 1;
+      } else if (todo.completed) {
+        completed[todo.userId] += 1;
+        }
+    });
+      console.log(completed);
   }
 });
